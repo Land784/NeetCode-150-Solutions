9165182a96a0e1b2665953c32e7487ecbce6ea84
Land784 <weschmidt15@icloud.com>
2025-11-12 23:39:40 -0500

    Add Two Sum solution with test main()

---
+++ Arrays and Hashing/Two Sum/main.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        for(int i = 0;i<nums.size();i++){
+            int diff = target - nums[i];
+            if(map.count(diff)){
+                return {map[diff],i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+int main() {
+    Solution s;
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    vector<int> result = s.twoSum(nums, target);
+
+    cout << "Indices: ";
+    for (int i : result) cout << i << " ";
+    cout << endl;
+
+    return 0;
+}
---
+++ TwoSum.h
@@ -0,0 +1,13 @@
+//
+// Created by William Schmidt on 11/12/25.
+//
+
+#ifndef NEETCODE_150_TWOSUM_H
+#define NEETCODE_150_TWOSUM_H
+
+
+class TwoSum {
+};
+
+
+#endif //NEETCODE_150_TWOSUM_H
